775f4344c10d3ef8826a56270a364a9e5005dd5c
Greg Berrett <grberrett@gmail.com>
2013-07-11 18:44:42 -0600

    Added player.cs file and tested an enum for cross class direction values

---
+++ LaserWalls/LaserWalls/Values.cs
@@ -0,0 +1,18 @@
+﻿// Used to hold constants, enums and other custom defined multiclass values.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LaserWalls
+{
+    public enum Direction
+    {
+        Up, Down, Left, Right
+    };
+
+    class Values
+    {
+    }
+}
